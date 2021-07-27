a8bc83a8d6304490db2e49a4bfb73fcc9b1a0a48
ed-dickinson <edwardejd@gmail.com>
2021-07-27 07:00:33 +0100

    month detail imp

--- controllers/vegController.js
+++ controllers/vegController.js
@@ -31,6 +31,49 @@ exports.veg_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Veg detail: ' + req.params.name);
 };
 
-exports.month_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Month list for ' + req.params.name);
+// exports.month_detail = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Month list for ' + req.params.name);
+// };
+
+exports.month_detail = function(req, res, next) {
+    // res.send('NOT IMPLEMENTED: month detail: ' + req.params.name);
+
+    async.waterfall([
+        function(callback) {
+            Month.findOne({ 'name': req.params.name })
+              .populate('month')
+              .exec(
+                function(err, month) {
+                  callback(null, month)
+                });
+        },
+
+        function(month, callback) {
+            Veg.find({ 'sow' : month.id})
+              .populate('veg')
+              .exec(
+                function(err, sow_veg){
+                  callback(null, {sow_veg, month});
+                });
+        },
+        function({sow_veg, month}, callback) {
+            Veg.find({ 'harvest' : month.id})
+              .populate('veg')
+              .exec(
+                function(err, harvest_veg){
+                  callback(null, {harvest_veg, sow_veg, month});
+                });
+        },
+
+    ],
+     function(err, results) {
+        if (err) { return next(err); }
+        if (results==null) { // No results.
+            var err = new Error('Month not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('month_detail', { title: results.month.name, month: results.month, sow_veg: results.sow_veg, harvest_veg: results.harvest_veg } );
+    });
 };
